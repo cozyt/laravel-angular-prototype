0eb5a6e067d4723d6a02c7a885cc864779b7ade7
CozyT <mail@cozyt.net>
2017-01-17 18:22:14 +0000

    Init commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'test@activeticketing.com',
+            'email' => 'test@activeticketing.com',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
